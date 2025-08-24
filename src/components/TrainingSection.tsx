6fe8f00801a66c9f104c9c7ee69b14a60f224059
WiseTales <zuhairtoons@gmail.com>
2025-08-24 17:44:54 +0530

    Update TrainingSection.tsx

--- src/components/TrainingSection.tsx
+++ src/components/TrainingSection.tsx
@@ -19,9 +19,181 @@ const TrainingSection = () => {
       openUrl: "https://drive.google.com/file/d/10j9s0CPFQFOpv1MW3VvF69K6-6K8f8TH/view?usp=sharing", 
       downloadUrl: "https://drive.google.com/uc?export=download&id=10j9s0CPFQFOpv1MW3VvF69K6-6K8f8TH"
     },
+    
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/10LJ5adywBLsfS9O82FD-9i46BPtUm0ts/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=10LJ5adywBLsfS9O82FD-9i46BPtUm0ts"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/11BemiHEDqhA1ZfL31JOc3Efw0mq-fSZp/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=11BemiHEDqhA1ZfL31JOc3Efw0mq-fSZp"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/12vhentDRgVZ9GRCbr10x6Rk3TpuJwQ6n/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=12vhentDRgVZ9GRCbr10x6Rk3TpuJwQ6n"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1B7elTFWHEFxA1KTRCpbuEeL2XWzrIcfL/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1B7elTFWHEFxA1KTRCpbuEeL2XWzrIcfL"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1Cf4GB6zydsm18nglHNsqbQTRU8zvYW49/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1Cf4GB6zydsm18nglHNsqbQTRU8zvYW49"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1GBLFp_W9Uw4HAPzNRrvZJFBl03j_Ofjp/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1GBLFp_W9Uw4HAPzNRrvZJFBl03j_Ofjp"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1Gl3D0Vrj94zyqQO6ZMRmxAXYx_GOMHxa/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1Gl3D0Vrj94zyqQO6ZMRmxAXYx_GOMHxa"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1GxeV140mzktsEZqrRjzhfSSEMOvkHrRM/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1GxeV140mzktsEZqrRjzhfSSEMOvkHrRM"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1I54IAnXJ3jKGXWFGbGG3ipaUyAtaWAPl/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1I54IAnXJ3jKGXWFGbGG3ipaUyAtaWAPl"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1L8SiIhh5rmvoqKMW9aSap6IOjDWQX4uj/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1L8SiIhh5rmvoqKMW9aSap6IOjDWQX4uj"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1LRyzQ7-aZNkN9_oBgIUeAGn7Qe01asCy/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1LRyzQ7-aZNkN9_oBgIUeAGn7Qe01asCy"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1OjPKqsKmHqTm2Pyvuie6qV-pGp_vojsP/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1OjPKqsKmHqTm2Pyvuie6qV-pGp_vojsP"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1P716LVipBBipcrSWvSWmiK6zwsFf_Z6W/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1P716LVipBBipcrSWvSWmiK6zwsFf_Z6W"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://docs.google.com/spreadsheets/d/1RJsOjA1_gU6C3oL8ZVdTPayAO8qnqJNm/edit?usp=sharing&ouid=117567334419080675185&rtpof=true&sd=true", 
+    downloadUrl: "https://docs.google.com/spreadsheets/d/1RJsOjA1_gU6C3oL8ZVdTPayAO8qnqJNm/export?format=xlsx"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1TGq-g53cO0GDUe9Wf08mm7tKRE79QRyT/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1TGq-g53cO0GDUe9Wf08mm7tKRE79QRyT"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1W3QGGv2vBv79KPmYgScL6cxUkVhPJ-en/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1W3QGGv2vBv79KPmYgScL6cxUkVhPJ-en"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1ZhQTzpkqM2Ffd9jTD5l-3quoX2_PSFVv/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1ZhQTzpkqM2Ffd9jTD5l-3quoX2_PSFVv"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://docs.google.com/spreadsheets/d/1ep42DG9BOLfXV8L8LBrLGr5-u2PpwaAa/edit?usp=sharing&ouid=117567334419080675185&rtpof=true&sd=true", 
+    downloadUrl: "https://docs.google.com/spreadsheets/d/1ep42DG9BOLfXV8L8LBrLGr5-u2PpwaAa/export?format=xlsx"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1h-D_zzsD87nJ8WMwh_NTaAtX2i8RwCTP/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1h-D_zzsD87nJ8WMwh_NTaAtX2i8RwCTP"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1l4Ud1Mtfw4w8eczYdutqLgLNpe8kP2kp/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1l4Ud1Mtfw4w8eczYdutqLgLNpe8kP2kp"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1tSz6GPxwpPAKCqf72kwOu9Jg_A-iOKAd/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1tSz6GPxwpPAKCqf72kwOu9Jg_A-iOKAd"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://docs.google.com/spreadsheets/d/1xxA19j2hwOVQAhj3Lu_sljkl4cpu0ffF/edit?usp=sharing&ouid=117567334419080675185&rtpof=true&sd=true", 
+    downloadUrl: "https://docs.google.com/spreadsheets/d/1xxA19j2hwOVQAhj3Lu_sljkl4cpu0ffF/export?format=xlsx"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://drive.google.com/file/d/1ypoCxU0VbidIuMEB1CDveXuIeR7B4zvd/view?usp=sharing", 
+    downloadUrl: "https://drive.google.com/uc?export=download&id=1ypoCxU0VbidIuMEB1CDveXuIeR7B4zvd"
+  },
+  {
+    title: "Official Circulars – Social Audit of Schools",
+    description: "Directive for conducting Social Audit in 14 districts, including planning, manpower, and timelines.",
+    developed: "Integral University, Lucknow – Social Audit Project, Samagra Shiksha",
+    openUrl: "https://docs.google.com/spreadsheets/d/1zMi65dsnfjlxilSifo7VwZOT13kPvtvY/edit?usp=sharing&ouid=117567334419080675185&rtpof=true&sd=true", 
+    downloadUrl: "https://docs.google.com/spreadsheets/d/1zMi65dsnfjlxilSifo7VwZOT13kPvtvY/export?format=xlsx"
+  }
+
+
+    
    
     // Add new entries here
-   
+    
    
   ];
 
